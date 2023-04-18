f7f254ce169296dd6d7c5a14275705b7bd3a0212
Vachesav11 <shadowsrespektq@gmail.com>
2023-04-18 22:19:21 +0300

    add header add home add list add cover

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from "./components/header/header";
+import Home from "./components/home/home";
+import List from  "./components/list/list";
+import Cover from "./components/cover/cover";
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <Home/>
+            <List/>
+            <Cover/>
+        </div>
+    );
+};
 
 export default App;
